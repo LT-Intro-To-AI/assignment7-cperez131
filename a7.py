73e945738fdb7f89b64276855060b02e9c0897a8
Christopher Perez <cperez131@cps.edu>
2023-04-14 09:40:30 -0500

    Added the party data table to the code Next is to find what party the voters in table 3 are in

--- a7.py
+++ a7.py
@@ -9,4 +9,17 @@ xor_data = [
 orn = NeuralNet(2, 3, 1)
 orn.train(xor_data)
 
-print(orn.test_with_expected(or_data))
+print(orn.test_with_expected(xor_data))
+
+party_data = [
+    ([0.9, 0.6, 0.8, 0.3, 0.1],[1.0]),
+    ([0.8, 0.8, 0.4, 0.6, 0.4],[1.0]),
+    ([0.7, 0.2, 0.4, 0.6, 0.3],[1.0]),
+    ([0.5, 0.5, 0.8, 0.4, 0.8],[0.0]),
+    ([0.3, 0.1, 0.6, 0.8, 0.8],[0.0]),
+    ([0.6, 0.3, 0.4, 0.3, 0.6],[0.0]),]
+
+partyn = NeuralNet(5, 3, 1)
+partyn.train(party_data)
+
+print(partyn.test_with_expected(party_data))
