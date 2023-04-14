2a27a1f5be7efc908590f8eb8bc8c46b7857106c
Christopher Perez <cperez131@cps.edu>
2023-04-14 08:59:51 -0500

    Found out how to make xor using the .train

--- a7.py
+++ a7.py
@@ -4,9 +4,9 @@ xor_data = [
     ([0,0],[0]),
     ([0,1], [1]),
     ([1,0], [1]),
-    ([1,1], [1]),]
+    ([1,1], [0]),]
 
 orn = NeuralNet(2, 3, 1)
 orn.train(xor_data)
 
-print(orn.test_with_expected(xor_data))
+print(orn.test_with_expected(or_data))
