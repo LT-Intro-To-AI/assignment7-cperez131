b78250206fdff472b230588f5e0f9991d2cf961f
Christopher Perez <cperez131@cps.edu>
2023-04-19 00:04:21 -0500

    Completed task 4 and task 3 Used the test function to find whether or not the data given were Democrats or Republican

--- a7.py
+++ a7.py
@@ -4,22 +4,55 @@ xor_data = [
     ([0,0],[0]),
     ([0,1], [1]),
     ([1,0], [1]),
-    ([1,1], [0]),]
+    ([1,1], [0]),
+]
 
 orn = NeuralNet(2, 3, 1)
 orn.train(xor_data)
 
 print(orn.test_with_expected(xor_data))
 
-party_data = [
-    ([0.9, 0.6, 0.8, 0.3, 0.1],[1.0]),
-    ([0.8, 0.8, 0.4, 0.6, 0.4],[1.0]),
-    ([0.7, 0.2, 0.4, 0.6, 0.3],[1.0]),
-    ([0.5, 0.5, 0.8, 0.4, 0.8],[0.0]),
-    ([0.3, 0.1, 0.6, 0.8, 0.8],[0.0]),
-    ([0.6, 0.3, 0.4, 0.3, 0.6],[0.0]),]
+xor2_data = [
+    ([0,0],[0]),
+    ([0,1], [1]),
+    ([1,0], [1]),
+    ([1,1], [0]),
+]
 
-partyn = NeuralNet(5, 3, 1)
-partyn.train(party_data)
+orn2 = NeuralNet(2, 1, 1)
+orn2.train(xor_data)
 
-print(partyn.test_with_expected(party_data))
+print(orn2.test_with_expected(xor2_data))
+
+# party_data = [
+#     ([0.9, 0.6, 0.8, 0.3, 0.1],[1.0]),
+#     ([0.8, 0.8, 0.4, 0.6, 0.4],[1.0]),
+#     ([0.7, 0.2, 0.4, 0.6, 0.3],[1.0]),
+#     ([0.5, 0.5, 0.8, 0.4, 0.8],[0.0]),
+#     ([0.3, 0.1, 0.6, 0.8, 0.8],[0.0]),
+#     ([0.6, 0.3, 0.4, 0.3, 0.6],[0.0]),]
+
+# partyn = NeuralNet(5, 6, 1)
+# partyn.train(party_data)
+
+# print(partyn.test_with_expected(party_data))
+
+# test_data = [
+#     [1, 1, 1, .1, .1],
+#     [.5, .2, .1, .7, .7],
+#     [.8, .3, .3, .3, .8],
+#     [.8, .3, .3, .8, .3],
+#     [.9, .8, .8, .3, .6]
+# ]
+
+# for x in range(5):
+#     print(f"case {x+1}: {test_data[x]} evaluates to: {partyn.evaluate(test_data[x])}")
+#     eval = partyn.evaluate(test_data[x])
+#     print(eval)
+#     # if int(partyn.evaluate(test_data[x]) )> .6:
+#     #     print(" Republican")
+#     # elif int(partyn.evaluate(test_data[x])) < .4:
+#     #     print(" Democratic")
+#     # else:
+#     #     print( "Neutral")
+
